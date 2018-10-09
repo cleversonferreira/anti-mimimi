fc27a8328af5561064b8684f8713343422e043be
Cleverson <cleverson_reisferreira@hotmail.com>
2018-10-09 14:54:11 -0300

    code improvements

--- script.js
+++ script.js
@@ -1,33 +1,4 @@
-var blacklist = [
-	'bolsonaro',
-	'haddad',
-	'ciro',
-	'marina',
-	'pt',
-	'psdb',
-	'psol',
-	'sulistas',
-	'politica',
-	'fascista',
-	'fascista',
-	'fascismo',
-	'militarismo',
-	'lula',
-	'dilma',
-	'gleisi',
-	'petista',
-	'manifestantes',
-	'#elenao',
-	'bancada',
-	'#elenunca',
-	'#haddahsim',
-	'amoedo',
-	'direita',
-	'esquerda',
-	'petralha',
-	'13',
-	'17',
-];
+var blacklist = ['bolsonaro','haddad','ciro','marina','pt','psdb','psol','sulistas','politica','fascista','fascista','fascismo','militarismo','lula','dilma','gleisi','petista','manifestantes','#elenao','bancada','#elenunca','#haddahsim','amoedo','direita','esquerda','petralha','13','17'];
 
 function checkElements(post) {
 	blacklist.forEach(function(word){
