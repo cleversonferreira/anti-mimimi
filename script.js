fb51641bb31bbb826a5dd9a5d838e518b217f5f5
Cleverson Franco <cleverson_reisferreira@hotmail.com>
2021-02-02 18:18:47 -0300

    Update script.js
    update to 2021

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-var blacklist = ['bolsonaro','bozonaro','haddad','bolsominion','bolsominions','ciro','amoedo','marina','lula','dilma','gleisi','pt','psdb','psol','mbl','sulistas','nordestinos','politica','fascistas','fascista','fascismo','militarismo','comunistas','socialista','socialismo','petista','manifestantes','#elenao','#elenão','#elejamais','#lulalivre','bancada','#elenunca','#haddahsim','direita','esquerda','petralha','cuba','13','17'];
+var blacklist = ['bolsonaro','bozonaro','haddad','bolsominion','bolsominions','ciro','amoedo','marina','lula','dilma','gleisi','pt','psdb','psol','mbl','sulistas','nordestinos','politica','fascistas','fascista','fascismo','militarismo','comunistas','socialista','socialismo','petista','manifestantes','#elenao','#elenão','#elejamais','#lulalivre','bancada','#elenunca','#haddahsim','direita','esquerda','petralha','cuba','13','17', 'bbb'];
 
 if (window.location.hostname === 'www.facebook.com'){
 	setInterval(function(){
 		document.querySelectorAll('[data-ad-preview]').forEach(function(post){
 			blacklist.forEach(function(word){
-				if(post.textContent.toLowerCase().indexOf(word) >= 0){
-					post.closest('._5jmm').remove();
-				}
+                		if(post.textContent.toLowerCase().indexOf(word) >= 0){
+				    post.style.border = "5px solid red"					
+				    post.closest('.du4w35lb').remove();
+				}				
 			});
 		});
 	}, 1000);
